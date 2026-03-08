b61b8d16fba821377ba6cfaec9ec4b70416c3d3f
hrzy <herzwhitelead@gmail.com>
2026-03-08 15:24:47 +0800

    Prog01

---
+++ find_the_larger_number.py
@@ -0,0 +1,12 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+# Input First Number
+num1 = float(input("Enter a number: "))
+# Input Second Number
+num2 = float(input("Enter another number: "))
+# Compare the 2 numbers
+if num1 == num2:
+    print("The two numbers are equal.")
+elif num1 > num2:
+    print(num1)
+else:
+    print(num2)
