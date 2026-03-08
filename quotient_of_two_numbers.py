bea16a86375eae1309188726ae604f25a88d3b4e
hrzy <herzwhitelead@gmail.com>
2026-03-08 15:54:44 +0800

    Prog05

---
+++ quotient_of_two_numbers.py
@@ -0,0 +1,7 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+#Input first number
+num = float(input("Enter number: "))
+#input second number and first number divided by second number
+num /= float(input("Enter another number: "))
+print(num)
+
