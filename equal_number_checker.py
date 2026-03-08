8ca9211fed8530cccaa18f7f7436cd242f476b26
hrzy <herzwhitelead@gmail.com>
2026-03-08 15:37:44 +0800

    Prog02

---
+++ equal_number_checker.py
@@ -0,0 +1,8 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+#Input First Number
+num1 = float(input("Enter a first number: "))
+#Input Second Number
+num2 = float(input("Enter a second number: "))
+#Check if the number is equal
+if num1 == num2:
+    print("Equal")
