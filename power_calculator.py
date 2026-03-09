d284ea2de090d19d5951c3c66a82c64fadfa83ec
hrzy <herzwhitelead@gmail.com>
2026-03-09 11:48:17 +0800

    Prog06

---
+++ power_calculator.py
@@ -0,0 +1,7 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+#Input first number
+num1 = int(input("Enter number: "))
+#input second number
+num2 = int(input("Enter the exponent: "))
+# the first number is raised to the second number
+print(num1 ** num2)
