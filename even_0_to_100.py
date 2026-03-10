1d3752a072a01309bef741c9f19f17d2e360ff23
hrzy <herzwhitelead@gmail.com>
2026-03-11 00:11:01 +0800

    I misunderstood the instruction.

--- even_0_to_100.py
+++ even_0_to_100.py
@@ -1,9 +1,7 @@
 #Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
-even_num = 0
 #print number o to 100
 for i in range (101):
-    # Identify if it's even then add one
+    # Identify if it's even then print
     if i % 2 == 0:
-        even_num += 1
-print(even_num)
+        print (i)
 
