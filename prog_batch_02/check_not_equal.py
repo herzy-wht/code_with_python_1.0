13d8f1c60c70ddb467eb4c3d0c8ae4594843060f
hrzy <herzwhitelead@gmail.com>
2026-03-11 01:23:41 +0800

    prog02

---
+++ prog_batch_02/check_not_equal.py
@@ -0,0 +1,11 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+#Input first number
+#Input first number
+num1 = float(input("Enter a number: "))
+#Input second number
+num2 = float(input("Enter another number: "))
+#Identify if it's equal or not
+if num1 == num2:
+    print()
+else:
+    print("Not Equal")
