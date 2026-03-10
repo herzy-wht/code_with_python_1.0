d05a638876646844eabf49cea7492fa0a1be0101
hrzy <herzwhitelead@gmail.com>
2026-03-11 01:27:37 +0800

    prog03

---
+++ prog_batch_02/diff_two_numbers.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+#Input first number
+num = float(input("Enter a number: "))
+#Input Second number
+#Find the difference
+num -= float(input("Enter another number: "))
+print(num)
