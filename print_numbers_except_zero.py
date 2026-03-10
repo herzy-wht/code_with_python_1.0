db57ebd78684319a4a8b6ab710fc765d43f6b74c
hrzy <herzwhitelead@gmail.com>
2026-03-11 00:31:20 +0800

    Use an if-else statement and make it less space

--- print_numbers_except_zero.py
+++ print_numbers_except_zero.py
@@ -1,7 +1,8 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 #Print 0 to 100
-for i in range (101):
+for i in range (1, 100):
     #Identify if the number is divisible by 10 then print nothing
     if i % 10 == 0:
-        print("\n")
-    print(i)
+        print(end='')
+    else:
+        print(i)
