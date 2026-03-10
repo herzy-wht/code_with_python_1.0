0ae0eddeeda3a4f12f30231b223c07ffc4326f87
hrzy <herzwhitelead@gmail.com>
2026-03-10 23:33:05 +0800

    Prog07:Initial Attempt

---
+++ sum_of_10_numbers.py
@@ -0,0 +1,9 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+#Input first number
+num1 = float(input("Enter the number: "))
+
+#Input next number
+for i in range(9):
+    next_num = float(input("Enter the next number: ")
+
+#Then add them
