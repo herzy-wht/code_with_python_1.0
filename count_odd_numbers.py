f31d98a7e06eff6387e4635dfe541348b807250e
hrzy <herzwhitelead@gmail.com>
2026-03-10 23:56:16 +0800

    Prog08

--- count_odd_numbers.py
+++ count_odd_numbers.py
@@ -1,11 +1,13 @@
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+odd_num = 0
+
 for i in range (10):
-#input number
-   num = int(input(f"Enter the number {i}:"))
-   odd_num = 0
-#identify if it's odd or not then plus 1
-   if num % 2 != 0:
-      odd_num += 1
+    #input number
+    num = int(input(f"Enter the number {i}: "))
+
+    #identify if it's odd or not then plus 1
+    if num % 2 != 0:
+        odd_num += 1
 
 #then the cycle continues until 10th number
 print(odd_num)
