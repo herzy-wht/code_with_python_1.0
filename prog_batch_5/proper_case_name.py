f2b94103a409d88b256dbe05e434c3946d2cf264
hrzy <herzwhitelead@gmail.com>
2026-03-11 22:34:50 +0800

    Prog05

---
+++ prog_batch_5/proper_case_name.py
@@ -0,0 +1,5 @@
+#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+#Input name
+name = str(input("Enter your name: "))
+#Print in capital letter
+print(name.title())
