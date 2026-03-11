772467fcd79c754cad1baf23ef3adb079b261c1b
hrzy <herzwhitelead@gmail.com>
2026-03-11 13:07:56 +0800

    Initial Attempt

---
+++ prog_batch_3/no_duplicates.py
@@ -0,0 +1,8 @@
+#Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+#Input 10 numbers
+for i in range(1,11):
+    num = int(input(f"Enter number {i}: "))
+    i = list
+#If the number is the same as the previous number, do not print it
+    if i not in list:
+        print(i)
