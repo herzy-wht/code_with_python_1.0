fcf6c2b626efb3f21d7ea3038ff52a9bd790224b
hrzy <herzwhitelead@gmail.com>
2026-03-11 13:25:46 +0800

    Prog01

---
+++ prog_batch_5/remove_leadind_spaces.py
@@ -0,0 +1,5 @@
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+#Input Name with spaces
+name = str(input("Enter your name:                  "))
+#Print name without spaces
+print("Name:", name)
