d025cf088b1747b115a096db1002e4174970f437
hrzy <herzwhitelead@gmail.com>
2026-03-11 22:32:21 +0800

    prog04

---
+++ prog_batch_5/lowercase_name.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+#Input name
+name = str(input("Enter your name: "))
+#Print in capital letter
+print(name.lower())
