583395775ce65d9894c42ddd014145b3baa0ae2e
hrzy <herzwhitelead@gmail.com>
2026-03-11 22:17:36 +0800

    input name

---
+++ prog_batch_5/uppercase_name.py
@@ -0,0 +1,6 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+#Input name
+name = str(input("Enter your name: "))
+#Print in capital letter
+
+
