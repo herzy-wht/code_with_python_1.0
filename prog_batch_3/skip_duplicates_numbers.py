255bd38cc2743dbff62334833f76388307a98712
hrzy <herzwhitelead@gmail.com>
2026-03-11 13:12:22 +0800

    Initial attempt

---
+++ prog_batch_3/skip_duplicates_numbers.py
@@ -0,0 +1,3 @@
+#Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+#Input 10 numbers
+#If the number is the same as the previous number, do not print it
