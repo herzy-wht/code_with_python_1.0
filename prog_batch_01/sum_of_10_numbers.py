fa68c7e284ee4746ad83440367a4fc304d45170a
hrzy <herzwhitelead@gmail.com>
2026-03-12 12:45:07 +0800

    prog07

--- prog_batch_01/sum_of_10_numbers.py
+++ prog_batch_01/sum_of_10_numbers.py
@@ -1,9 +1,8 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 #Input first number
-num1 = float(input("Enter the number: "))
-
+num = float(input("Enter the number: "))
 #Input next number
-for i in range(9):
-    next_num = float(input("Enter the next number: ")
-
 #Then add them
+for i in range(9):
+    num += float(input("Enter the next number: "))
+print(num)
