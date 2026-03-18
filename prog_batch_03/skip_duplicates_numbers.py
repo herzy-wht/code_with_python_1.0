a3b792e56d9beb49c1fbc778fe62d61727c8efa6
hrzy <herzwhitelead@gmail.com>
2026-03-18 22:32:39 +0800

    prog02

--- prog_batch_03/skip_duplicates_numbers.py
+++ prog_batch_03/skip_duplicates_numbers.py
@@ -1,3 +1,15 @@
 #Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+numbers = []
+unique_numbers = []
 #Input 10 numbers
+for i in range(1,11):
+    num = int(input(f"Enter number {i}: "))
+    numbers.append(num)
 #If the number is the same as the previous number, do not print it
+for num in numbers:
+    if num not in unique_numbers:
+        unique_numbers.append(num)
+for num in unique_numbers:
+    print(num, end=" ")
+
+
