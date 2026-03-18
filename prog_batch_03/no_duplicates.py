c072e47874fff4e870eaee1d205840aefe5150e0
hrzy <herzwhitelead@gmail.com>
2026-03-18 21:57:27 +0800

    change the condition in finding duplicates num

--- prog_batch_03/no_duplicates.py
+++ prog_batch_03/no_duplicates.py
@@ -1,11 +1,12 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-list = []
+numbers = []
 #Input 10 numbers
 for i in range(1,11):
     num = int(input(f"Enter number {i}: "))
-    list.append(num)
-#If the number is the same as the previous number, do not print it
-    for j in list:
-        if list != list[j]:
-             print(j)
+    numbers.append(num)
+#If the number is only one in the set, print it
+for num in numbers:
+    if numbers.count(num) == 1:
+        print(num, end=" ")
+
 
