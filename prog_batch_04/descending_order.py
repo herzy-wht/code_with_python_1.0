559cc16bc32310ddba94c4a5feefb33a6038cd70
hrzy <herzwhitelead@gmail.com>
2026-03-20 20:13:55 +0800

    prog04

---
+++ prog_batch_04/descending_order.py
@@ -0,0 +1,15 @@
+#Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+
+numbers.sort(reverse=True)
+
+print("Numbers from highest to lowest:")
+for n in numbers:
+    print(n)
