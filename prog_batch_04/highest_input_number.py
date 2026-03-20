727c7912746b7f8086178b91553f60fa69746169
hrzy <herzwhitelead@gmail.com>
2026-03-20 20:12:27 +0800

    prog03

---
+++ prog_batch_04/highest_input_number.py
@@ -0,0 +1,11 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+
+print("Highest number:", max(numbers))
