03bb44f2e15f77c8a7a581fe3b15311f1bdd7868
hrzy <herzwhitelead@gmail.com>
2026-03-20 20:20:06 +0800

    change comment

--- prog_batch_05/reverse_casing.py
+++ prog_batch_05/reverse_casing.py
@@ -1,5 +1,4 @@
-#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
-#Input name
+#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 name = str(input("Your Name: "))
 #if the letter is lowercase make it uppercase and vise versa
 for letter in name:
